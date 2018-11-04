98d2d7c331445a2435b20e1c574ade97b1f0dc6a
Amril Syaifa Yasin <amrilsyaifa@gmail.com>
2018-11-04 14:56:08 +0700

    .reduce .concat javascript

--- src/components/Layout/Burger/Burger.js
+++ src/components/Layout/Burger/Burger.js
@@ -4,14 +4,19 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.css';
 
 const burger = (props) => {
-  const tes = Object.keys(props.ingredients);
-  console.log(tes);
-  const transformIngredients = Object.keys(props.ingredients).map((igKey) => {
-    return [ ...Array(props.ingredients[igKey]) ].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
-  console.log(transformIngredients);
+  let transformIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [ ...Array(props.ingredients[igKey]) ].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((prev, next) => {
+      // reduce berfungsi untuk menjumlahkan nilai array
+      return prev.concat(next); //concat() adalah method yang digunakan untuk menggabungkan dua atau lebih array menjadi sebuah array.
+    }, []);
+  if (transformIngredients.length === 0) {
+    transformIngredients = <p>Please Start Adding ingredients</p>;
+  }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type='bread-top' />
