ad5e2c6a913873a8eb0dae82e9ddc893c1ba1ff1
Amril Syaifa Yasin <amrilsyaifa@gmail.com>
2018-11-04 14:25:54 +0700

    How to change Object in State to Array

--- src/components/Layout/Burger/Burger.js
+++ src/components/Layout/Burger/Burger.js
@@ -4,13 +4,18 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.css';
 
 const burger = (props) => {
+  const tes = Object.keys(props.ingredients);
+  console.log(tes);
+  const transformIngredients = Object.keys(props.ingredients).map((igKey) => {
+    return [ ...Array(props.ingredients[igKey]) ].map((_, i) => {
+      return <BurgerIngredient key={igKey + i} type={igKey} />;
+    });
+  });
+  console.log(transformIngredients);
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type='bread-top' />
-      <BurgerIngredient type='cheese' />
-      <BurgerIngredient type='bacon' />
-      <BurgerIngredient type='salad' />
-      <BurgerIngredient type='meat' />
+      {transformIngredients}
       <BurgerIngredient type='bread-bottom' />
     </div>
   );
