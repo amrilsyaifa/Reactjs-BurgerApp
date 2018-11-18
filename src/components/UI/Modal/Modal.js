6c90eab6763f67a3320b428e27bae35a932ab274
amril syaifa yasin <amrilsyaifa@gmail.com>
2018-11-18 14:22:36 +0700

    Add Backdrop close

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import AUX from '../../../hoc/Auxs';
+import Backdrop from '../Backdrop/Backdrop';
 
 import classes from './Modal.css';
 
 const modal = (props) => (
-  <div className={classes.Modal} 
-  style={{ 
-      transform: props.show ? 'translateY(0)' : 'translateY(-100vh)', 
-      opacity: props.show ? '1' : '0' }}>
-    {props.children}
-  </div>
+  <AUX>
+    <Backdrop show={props.show} clicked={props.modalClose} />
+    <div
+      className={classes.Modal}
+      style={{
+        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+        opacity: props.show ? '1' : '0',
+      }}
+    >
+      {props.children}
+    </div>
+  </AUX>
 );
 
 export default modal;
